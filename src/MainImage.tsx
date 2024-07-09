2ceaaa1af7b76c8930eba7201a6865dd77e042b0
minhyepark <minhyepark0572@gmail.com>
2024-07-09 15:51:58 +0900

    edit: lottie 기능 추가

--- src/MainImage.tsx
+++ src/MainImage.tsx
@@ -1,10 +1,17 @@
 import styled from '@emotion/styled';
 import mainImg from './assets/img1.jpg';
 import { css } from '@emotion/react';
+import Lottie from 'lottie-react';
+// import Sample from './assets/sample.json';
+// import Sample2 from './assets/sample2.json';
+import Sample3 from './assets/sample3.json';
 
 const MainImage = () => {
   return (
     <StyledMainImg>
+      <LottieContainer>
+        <Lottie animationData={Sample3} loop />
+      </LottieContainer>
       <img src={mainImg} alt="main" />
       <StyledTitle>
         <p>날짜</p>
@@ -17,6 +24,7 @@ const MainImage = () => {
 export default MainImage;
 
 const StyledMainImg = styled.div`
+  position: relative;
   img {
     width: 100%;
   }
@@ -35,4 +43,13 @@ const StyledTitle = styled.div`
       ${theme.typography.content1Bold}
     `}
   }
+`;
+
+const LottieContainer = styled.div`
+  width: 100%;
+  height: 500px;
+  margin: 0 auto;
+  position: absolute;
+  top: 0;
+  z-index: 1000;
 `;
