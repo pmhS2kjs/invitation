234d0e7c6f7c8e398a32d6332d61ce92140e202f
minhyepark <minhyepark0572@gmail.com>
2024-07-11 00:52:09 +0900

    edit: change swiper option and img

--- src/MainGallery.tsx
+++ src/MainGallery.tsx
@@ -3,32 +3,44 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import styled from '@emotion/styled';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import Img2 from './assets/imgs/img2.jpg';
+import Img3 from './assets/imgs/img3.jpg';
+import Img4 from './assets/imgs/img4.jpg';
+import Img5 from './assets/imgs/img5.jpg';
 
 import { Pagination } from 'swiper/modules';
 
 const MainGallery = () => {
   return (
-    <InnerLayout>
-      <h2>갤러리</h2>
+    <InnerLayout layout="full">
       <Swiper
         loop
         pagination={{
           dynamicBullets: true,
         }}
+        autoHeight={true}
         modules={[Pagination]}
         className="mySwiper"
       >
         <SwiperSlide>
-          <StyledTest>test1</StyledTest>
+          <StyledImg>
+            <img src={Img2} alt="" />
+          </StyledImg>
         </SwiperSlide>
         <SwiperSlide>
-          <StyledTest>test2</StyledTest>
+          <StyledImg>
+            <img src={Img3} alt="" />
+          </StyledImg>
         </SwiperSlide>
         <SwiperSlide>
-          <StyledTest>test3</StyledTest>
+          <StyledImg>
+            <img src={Img4} alt="" />
+          </StyledImg>
         </SwiperSlide>
         <SwiperSlide>
-          <StyledTest>test4</StyledTest>
+          <StyledImg>
+            <img src={Img5} alt="" />
+          </StyledImg>
         </SwiperSlide>
       </Swiper>
     </InnerLayout>
@@ -37,8 +49,11 @@ const MainGallery = () => {
 
 export default MainGallery;
 
-const StyledTest = styled.div`
+const StyledImg = styled.div`
   width: 100%;
-  height: 300px;
-  background-color: red;
+  display: flex;
+  align-items: center;
+  img {
+    width: 100%;
+  }
 `;
