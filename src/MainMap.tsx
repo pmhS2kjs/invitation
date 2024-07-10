7804d7921c7946aa64f9cc4b831e7bdeae05a7cd
minhyepark <minhyepark0572@gmail.com>
2024-07-11 00:54:01 +0900

    edit: 대중교통 추가

--- src/MainMap.tsx
+++ src/MainMap.tsx
@@ -1,4 +1,6 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import InnerLayout from '@src/InnerLayout';
 import { useEffect, useRef } from 'react';
 
 const MainMap = () => {
@@ -49,10 +51,43 @@ const MainMap = () => {
 
   return (
     <>
-      <div ref={mapContainerRef} style={{ width: '100%', height: '500px' }} />
-      <StyledInfo>
-        <p>버스</p>
-      </StyledInfo>
+      <div
+        ref={mapContainerRef}
+        style={{ width: '100%', height: '300px', backgroundColor: 'lightblue' }}
+      />
+      <InnerLayout>
+        <StyledInfo>
+          <div>
+            <h3>택시</h3>
+            <p>마산역 택시승강장 탑승 시 10분 소요</p>
+          </div>
+          <div>
+            <h3>버스</h3>
+            <div>
+              <p>어느정류장 하차</p>
+              <p>107, 114, 116, 160, 162, 163, 164, 257, 740,3002</p>
+            </div>
+            <div>
+              <p>봉안공단 입구 하차</p>
+              <p>162</p>
+            </div>
+            <div>
+              <p>(주)동방 하차</p>
+              <p>540</p>
+            </div>
+          </div>
+          <div>
+            <h3>주차안내</h3>
+            <div>
+              <p>
+                내비게이션 '힐스카이 웨딩&컨벤션' 또는 '마산회원구 봉양로 133'
+              </p>
+              <p>2층 ~ 6층 주차장 무료 이용</p>
+              <p>동방창원지사 주차장 / 2시간 무료</p>
+            </div>
+          </div>
+        </StyledInfo>
+      </InnerLayout>
     </>
   );
 };
@@ -61,4 +96,20 @@ export default MainMap;
 
 const StyledInfo = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 32px;
+  text-align: left;
+  h3 {
+    margin-bottom: 16px;
+    ${({ theme }) => css`
+      color: ${theme.color.gray800};
+      ${theme.typography.content1Bold}
+    `}
+  }
+  div > div {
+    margin-bottom: 10px;
+    p:first-of-type {
+      margin-bottom: 8px;
+    }
+  }
 `;
