8fa80f079887be8475215c65cf6b0319fde7b88c
minhyepark <minhyepark0572@gmail.com>
2024-07-11 01:00:01 +0900

    edit: footer styles

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,18 @@
+import styled from '@emotion/styled';
+
 const Footer = () => {
   return (
-    <footer>
+    <StyledFooter>
       <p>© 2024 Minhye Park. All rights reserved.</p>
-    </footer>
+    </StyledFooter>
   );
 };
 
 export default Footer;
+
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 32px 0;
+`;
